b8fdbec1782a0b1181496a60368a5dca02b86cf4
hsnsas <hassenjiwad642@gmail.com>
2023-02-18 19:52:07 -0800

    prints all single digit numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - a program that prints all single digit numbers of base ten
+ * Description: Starting from zero
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char c = 0;
+
+	while (c < 10)
+	{
+	putchar(48 + c);
+	c++;
+	}
+	putchar('\n');
+	return (0);
+}

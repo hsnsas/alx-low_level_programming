3261afdf776e36996fa53d1249faceff21112d65
hsnsas <hassenjiwad642@gmail.com>
2023-02-20 14:53:05 -0800

    UNIX is basically a simple operating system

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+/**
+ * main - a C program that prints exactly
+ * Description: and that piece of art is useful
+ * Return: 1 (success)
+ */
+#include <stdio.h>
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}

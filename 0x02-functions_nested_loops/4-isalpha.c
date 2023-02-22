dae2584a551f88cd13b3718a76d3d71eb715032f
hsnsas <hassenjiwad642@gmail.com>
2023-02-22 10:42:19 -0800

    checks for alphabetic character.

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,7 +2,7 @@
 
 /**
  * _isalpha - checks for alphabetic character
- *
+ * @c: is simply a letter
  * Description: a function that checks for alphabetic character.
  * Return: 1 if c is a letter, lowercase or uppercase 0 otherwise
  */

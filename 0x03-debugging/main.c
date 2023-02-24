64dc537bf521f049677a05ce31d017a5907e711b
hsnsas <hassenjiwad642@gmail.com>
2023-02-23 16:26:20 -0800

    Like, comment, subscribe

---
+++ 0x03-debugging/main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - tests function that prints integer posetive or negative
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 98;
+	posetive_or_negative(i);
+
+	return (0);
+}
